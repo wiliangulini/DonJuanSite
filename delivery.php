14d67bec0140726d9f69ebf7d40e06a09873df7d
wiliangulini <tochacannabis@gmail.com>
2020-01-19 05:15:54 -0300

    algumas alterações no delivery, informações sobre a entrega, uma leve pegada na escolha de pães e algumas edições no css.

--- delivery.php
+++ delivery.php
@@ -1,5 +1,24 @@
 <?php require "includes/menu.php" ?>
 
-bbbbbb
+<div class="delivery">
+	  <div class="deliveryint">
+	  	<div class="information">
+	  		<p class="p">Tempo de Entrega: 30 a 60min. **</p>
+	  		<p>Taxa de entrega: R$8,00 a R$15,00 de acordo com a distância</p>
+	  		<p>Horário de atendimento: 18:30 as 23:30</p>
+	  		<p class="p">Horário de entrega: 19:00 as 23:15</p>
+	  	</div>
+	  	<div class="breads">
+	  		<p> Escolha seu pão:</p>
+	  		<select name="opções">
+	  			<option value="1">Crocrante</option>
+	  			<option value="2">Leite</option>
+	  			<option value="3">Brioche</option>
+	  			<option value="4">Australiano</option>
+	  		</select>
+
+	  	</div>
+	  </div>
+</div>
 
 <?php require "includes/footer.php" ?>
